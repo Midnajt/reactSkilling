0113bb20cbb04df52276942b7ea9b257a121f97f
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-10-23 16:41:22 +0200

    event handlers added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import ResultTable from "./components/ResultTable/ResultTable";
 import UserInput from "./components/UserInput/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  if (userInput) {
     const yearlyData = [];
 
     let currentSavings = +userInput["current-savings"];
     const yearlyContribution = +userInput["yearly-contribution"];
     const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
+    const duration = +userInput["duration-time"];
 
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
@@ -21,12 +28,12 @@ function App() {
         yearlyContribution: yearlyContribution,
       });
     }
-  };
+  }
 
   return (
     <div>
       <Header />
-      <UserInput />
+      <UserInput onCalculate={calculateHandler} />
       <ResultTable />
     </div>
   );
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,14 +1,58 @@
-const UserInput = () => {
+import React, { useState } from "react";
+
+const INITIAL_USER_INPUT = {
+  "current-savings": 10000,
+  "yearly-contribution": 1200,
+  "expected-return": 7,
+  "duration-time": 10,
+};
+
+const UserInput = (props) => {
+  const [userInput, setUserInput] = useState(INITIAL_USER_INPUT);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    props.onCalculate(userInput);
+  };
+
+  const resetHandler = () => {
+    setUserInput(INITIAL_USER_INPUT);
+  };
+
+  const changeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
+  };
+
   return (
-    <form className="form">
+    <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={(e) => {
+              changeHandler("current-savings", e.target.value);
+            }}
+            value={userInput["current-savings"]}
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={(e) => {
+              changeHandler("yearly-contribution", e.target.value);
+            }}
+            value={userInput["yearly-contribution"]}
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,15 +60,29 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            onChange={(e) => {
+              changeHandler("expected-return", e.target.value);
+            }}
+            value={userInput["expected-return"]}
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            onChange={(e) => {
+              changeHandler("duration-time", e.target.value);
+            }}
+            value={userInput["duration-time"]}
+            type="number"
+            id="duration-time"
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
