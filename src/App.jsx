1b2a1060492ba15758bc1672994f5e18b9f779d4
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-12-12 16:52:16 +0100

    modal component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import ProjectSidebar from "./components/ProjectSidebar";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    project: [],
+    projects: [],
   });
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState((prevState) => {
@@ -18,27 +27,29 @@ function App() {
     });
   }
 
-  function handleAddProject(project) {
+  function handleAddProject(projectData) {
     setProjectsState((prevState) => {
-      const newProject = { ...project, id: Math.random() };
+      const newProject = { ...projectData, id: Math.random() };
 
-      return { ...prevState, projects: [...prevState.project, newProject] };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-function NewProject({ onAdd }) {
+function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,6 +14,11 @@ function NewProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
+    if (enteredTitle.trim() === "" || enteredDescription.trim() === "" || enteredDueDate.trim() === "") {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -19,23 +27,32 @@ function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        </li>
-        <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title"></Input>
-        <Input ref={description} label="Description" textarea></Input>
-        <Input type="date" ref={dueDate} label="Due Date"></Input>
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4"> Oops ... looks like yout forgot to enter a vlue.</p>
+        <p className="text-stone-600 mb-4">Please make sure You provide valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title"></Input>
+          <Input ref={description} label="Description" textarea></Input>
+          <Input type="date" ref={dueDate} label="Due Date"></Input>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
