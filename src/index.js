0b6bde3ab176ec5b3eecc97c9b044e85dae4afdc
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-03-13 14:12:41 +0100

    starting setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactDOM from 'react-dom/client';
 
-import "./index.css";
-import App from "./App";
-import store from "./store";
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  //5. ustawiamy zaimportowany store z /store/index.js do Providera z react-redux
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
