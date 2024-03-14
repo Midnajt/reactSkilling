9006f40a43fbd310bd9ec8046ec5a62d732fc7d4
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-03-14 15:52:39 +0100

    refresher cd

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,22 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
   },
-  reducer: {
-    additemToCart(state, action) {
+  reducers: {
+    addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
         state.items.push({ itemId: newItem.id, price: newItem.price, quantity: 1, totalPrice: newItem.price, name: newItem.title });
       } else {
         existingItem.quantity++;
-        existingItem.totalprice = existingItem.totalprice + newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart(state) {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
