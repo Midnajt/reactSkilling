b805ec670890bcde1ce5bec44957b56fb2275ff6
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-03-19 10:57:40 +0100

    connecting with firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -5,6 +7,15 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart); //nasłuchujemy zmian w cart z reduxa
+
+  useEffect(() => {
+    console.log("App.js->useEffect start");
+    fetch("https://http-practices-e3d22-default-rtdb.europe-west1.firebasedatabase.app//cart.json", { method: "PUT", body: JSON.stringify(cart) });
+    console.log("App.js->useEffect finish");
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,14 +1,19 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
-  function addToCartHandler() {
+  const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
     dispatch(
       cartActions.addItemToCart({
         id,
@@ -16,7 +21,7 @@ const ProductItem = (props) => {
         price,
       })
     );
-  }
+  };
 
   return (
     <li className={classes.item}>
